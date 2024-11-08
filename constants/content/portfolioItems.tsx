0ea166e9aab4806c231d5a0d4fa720e1e39b6e80
Zimoo343 <davidruiz7.dev@gmail.com>
2024-11-08 16:17:29 -0600

    Update portfolioItems.tsx

--- constants/content/portfolioItems.tsx
+++ constants/content/portfolioItems.tsx
@@ -3,11 +3,11 @@ import { skills as sk } from "./skills";
 
 export const portfolioItems: PortfolioItemProps[] = [
   {
-    title: "ZtekAdminPanel",
+    title: "EzTip",
     videoUrl: "/videos/eztip.mp4",
     imageUrl: "/videos/eztip.webp",
     description:
-      "Worked on the donation-page of the platform, which allows creators to receive support through Ethereum and smart contracts.",
+      "Worked on the donation-page form and profile view, which allows creators to receive support through Ethereum and smart contracts.",
     link: "https://www.eztip.xyz",
     stack: [sk.jsts, sk.react, sk.next, sk.solidity, sk.tw],
   },
@@ -16,7 +16,7 @@ export const portfolioItems: PortfolioItemProps[] = [
     videoUrl: "/videos/ztekadminpanel.mp4",
     imageUrl: "/videos/ztekadminpanel.webp",
     description:
-      "Worked on the invoice form, autosend feature, and the dashboard of the platform as well as the UI for the platform.",
+      "Worked on the invoice form, autosend feature, and the dashboard as well as the UI for the platform.",
     stack: [sk.jsts, sk.react, sk.tw],
   },
 ];
