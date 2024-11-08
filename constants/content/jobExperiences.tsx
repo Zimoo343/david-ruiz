ca4d2bd39bcf5459dbe6e717ce13869d0c2577b5
Zimoo343 <davidruiz7.dev@gmail.com>
2024-11-08 16:40:44 -0600

    Update jobExperiences.tsx

--- constants/content/jobExperiences.tsx
+++ constants/content/jobExperiences.tsx
@@ -7,7 +7,7 @@ export const jobExperiences: JobExperienceProps[] = [
     companyName: "Ztek Interactive, LLC",
     startDate: "August 2023",
     endDate: "now",
-    location: "Remote, USA",
+    location: "Austin, Tx (Remote)",
     children:
       "At Ztek Interactive, I work as a Software Engineer, specializing in frontend development and UI/UX design with a focus on TypeScript. My projects include the Ztek Admin Panel, a platform for managing legal documents, invoices, and meetings, primarily built using TypeScript, Tailwind, CSS, and Prisma DB for data handling. Additionally, I’ve developed EzTip, a web3 donation platform that allows creators to receive support through Ethereum and smart contracts, also leveraging TypeScript, Tailwind, and blockchain technologies. My role involves designing efficient, user-friendly interfaces and providing backend support to create cohesive, secure applications.",
   },
