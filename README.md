508e42d27c0a45397a1e2981922671a1b24f8137
Zimoo343 <166975990+Zimoo343@users.noreply.github.com>
2026-09-08 14:08:54 -0600

    fix: update experience to 3 years everywhere
    - Updated resumeStatement ES/EN

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 💫 About Me:
 
-I'm a Software Engineer with 2 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I’m passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.
+I'm a Software Engineer with 3 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I’m passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.
 
 ## 🌐 Socials:
 
--- constants/content/index.ts
+++ constants/content/index.ts
@@ -78,9 +78,9 @@ export const content: { en: LanguageContent; es: LanguageContent } = {
       lastName: "Lara", // Last name just for CV generation
       jobTitle: "Software Engineer",
       statement:
-        "I'm a Software Engineer with 2 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
+        "I'm a Software Engineer with 3 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
       resumeStatement:
-        "I'm a Software Engineer with 2 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
+        "I'm a Software Engineer with 3 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
       location: "Los Reyes de Salgado, Michoacán. México",
     },
     skills: SKILLS,
@@ -236,7 +236,7 @@ export const content: { en: LanguageContent; es: LanguageContent } = {
     seo: {
       title: "David Ruiz | Software Engineer",
       description:
-        "I'm a Software Engineer with 2 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
+        "I'm a Software Engineer with 3 years of experience in web development, using technologies such as React, Next.js, CSS, and Tailwind. Skilled in working with SQL databases and experienced with modern tools like Prisma ORM and TRPC. I'm passionate about creating efficient, functional, and visually appealing user interfaces, with a strong focus on optimization and delivering an exceptional user experience in every project.",
     },
     ui: {
       experience: "Experience",
@@ -264,9 +264,9 @@ export const content: { en: LanguageContent; es: LanguageContent } = {
       lastName: "Lara", // Apellido solo para generación de CV
       jobTitle: "Ingeniero de Software",
       statement:
-        "Soy Ingeniero de Software con 2 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
+        "Soy Ingeniero de Software con 3 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
       resumeStatement:
-        "Soy Ingeniero de Software con 2 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
+        "Soy Ingeniero de Software con 3 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
       location: "Los Reyes de Salgado, Michoacán. México",
     },
     skills: SKILLS,
@@ -423,7 +423,7 @@ export const content: { en: LanguageContent; es: LanguageContent } = {
     seo: {
       title: "David Ruiz | Ingeniero de Software",
       description:
-        "Soy Ingeniero de Software con 2 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
+        "Soy Ingeniero de Software con 3 años de experiencia en desarrollo web, usando tecnologías como React, Next.js, CSS y Tailwind. Hábil en el trabajo con bases de datos SQL y con experiencia en herramientas modernas como Prisma ORM y TRPC. Me apasiona crear interfaces de usuario eficientes, funcionales y visualmente atractivas, con un fuerte enfoque en la optimización y en brindar una experiencia de usuario excepcional en cada proyecto.",
     },
     ui: {
       experience: "Experiencia",
